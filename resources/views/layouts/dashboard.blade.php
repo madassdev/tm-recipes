639feb559dc51ea90118b457c42455858e7a10a7
lordfaves <favescsskr@gmail.com>
2021-02-25 01:57:08 +0100

    enforce https

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,6 +11,12 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <script>
+        if (location.protocol !== 'https:' && location.hostname != 'localhost') {
+            location.replace(`https:${location.href.substring(location.protocol.length)}`);
+        }
+    </script>
     @yield('links')
 
     <title>Recipe</title>
