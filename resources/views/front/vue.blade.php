f90b65762c2ab2213c97ff11ac2524e9abd18a5f
lordfaves <favescsskr@gmail.com>
2021-04-07 12:42:35 +0100

    Production build

--- resources/views/front/vue.blade.php
+++ resources/views/front/vue.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="{{ asset('css/modal.css') }}" />
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
         <script src="{{ asset('js/app.js') }}"></script>
         <link
             rel="stylesheet"
